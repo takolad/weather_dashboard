ad1227a5087cc46af021fa61a04311261ce46045
John Hinojosa <hinojohn@ymail.com>
2021-03-06 01:55:29 -0600

    All data from current weather now displays properly, minor changes to style and index

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,8 @@ var currTempFieldEl = $('.temperature');
 var currCityDateFieldEl = $('.cityDate');
 var currHumidFieldEl = $('.humidity');
 var currWindFieldEl = $('.wind');
-var currUvIndFieldEl = $('.uv');
+var currUvIndFieldEl = $('.uvIndex');
+var currWeatherIconEl = $('weatherIcon');
 
 
 var key = config.OWM_API_KEY;
@@ -14,18 +15,22 @@ var coord = {
     lat: 0,
 };
 var weather = {
-    icon: "",   // Doesn't work atm
+    icon: "",
+    desc: "",
 };
 var main = {
-    temp: 0, // fahrenheit
-    humidity: 0,    //  Doesn't work atm
+    temp: 0,
+    humidity: 0,
 };
 var wind = {
     speed: 0,
 };
 var uvIndex = {
-    uvi: 0, //  Doesn't work atm
+    uvi: 0,
 };
+
+// Time of data calculation, unix, UTC
+var dt = 0;
 
 var fiveDayStats = [
     {
@@ -74,7 +79,7 @@ var apiFiveDayUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityNa
 // list[i].main.temp
 // list[i].main.humidity
 // list[i].weather.icon
-var apiIconUrl = `http://openweathermap.org/img/wn/${weather.icon}@2x.png`;
+var iconUrl = `http://openweathermap.org/img/wn/${weather.icon}@2x.png`;
 
 var formSearchEl = $('.searchForm');
 
@@ -100,8 +105,10 @@ function getCurrentWeather() {
                     main.temp = data.main.temp;
                     main.humidity = data.main.humidity;
                     wind.speed = data.wind.speed;
+                    dt = data.dt;
                     apiUvUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coord.lat}&lon=${coord.lon}&exclude={minutely,hourly,daily,alerts}&units=imperial&appid=${key}`;
-                    apiIconUrl = `http://openweathermap.org/img/wn/${weather.icon}@2x.png`;
+                    iconUrl = `http://openweathermap.org/img/wn/${weather.icon}@2x.png`;
+                    weather.desc = data.weather[0].description;
                     getUvIndex(coord.lat, coord.lon);
                     displayCurrWeather();   // simply logs to console atm
                 });
@@ -132,14 +139,30 @@ function getUvIndex(latitude, longitude) {
 }
 
 function displayCurrWeather() {
-    // console.log("coords: lon: " + coord.lon + ", lat: " + coord.lat);
-    // console.log("current iconId: " + weather.icon);
-    // console.log("current temp: " + main.temp);
-    // console.log("current humidity: " + main.humidity);
-    // console.log("current wind: " + wind.speed);
-    // console.log("current uvIndex: " + uvIndex.uvi);
-    currCityDateFieldEl.val(`${cityName} (${Date.now()})`);
-    currUvIndFieldEl.removeClass('d-none');
+    var dtInMs = dt * 1000;
+    const dateObject = new Date(dtInMs);
+    var todaysDate = dateObject.toLocaleDateString({month: "numeric", day: "numeric", year: "numeric"});
+    currCityDateFieldEl.text(`${cityName} (${todaysDate})`);
+    currCityDateFieldEl.append($('<img>').attr({'src':`${iconUrl}`,'alt':`${weather.desc}`}).css('width','50px'));
+    currTempFieldEl.text(`Temperature: ${main.temp} \u00B0F`);
+    currHumidFieldEl.text(`Humidity: ${main.humidity}%`);
+    currWindFieldEl.text(`Wind Speed: ${wind.speed} MPH`);
+    currUvIndFieldEl.text('UV Index: ');
+    let bgColor = '';
+    if (uvIndex.uvi >= 0 && uvIndex.uvi < 3) {
+        bgColor = 'green';
+    } else if (uvIndex.uvi >= 3 && uvIndex.uvi < 6) {
+        bgColor = 'yellow'
+    } else if (uvIndex.uvi >= 6 && uvIndex.uvi < 8) {
+        bgColor = 'orange';
+    } else if (uvIndex.uvi >= 8 && uvIndex.uvi < 11) {
+        bgColor = 'red';
+    } else {
+        bgColor = 'purple';
+    }
+    currUvIndFieldEl.append($('<span>').addClass('uv').css('background-color',`${bgColor}`));
+    $('.uv').text(`${uvIndex.uvi}`);
+
 }
 
 
